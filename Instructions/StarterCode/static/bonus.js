5f906a2c577a190bd513a7ae7e3f505657b037c3
Tejas Patel <Tejas@Tejass-MacBook-Pro.local>
2021-05-14 19:18:37 -0400

    HW15

---
+++ Instructions/StarterCode/static/bonus.js
@@ -0,0 +1,122 @@
+// BONUS: Build the Gauge Chart
+ 
+
+// Color palette for Gauge Chart
+var arrColorsG = ["#5899DA", "#E8743B", "#19A979", "#ED4A7B", "#945ECF", "#13A4B4", "#525DF4", "#BF399E", "#6C8893", "white"];
+
+
+// Use`d 3.json` to fetch the metadata for a sample
+// Hint: Inside the loop, you will need to use d3 to append new
+// tags for each key-value in the metadata.
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata= data.metadata;
+    var resultsarray= metadata.filter(sampleobject => 
+      sampleobject.id == sample);
+    var result= resultsarray[0]
+// Use d3 to select the panel with id of `#sample-metadata`
+    var panel = d3.select("#sample-metadata");
+// Use `.html("") to clear any existing metadata
+    panel.html("");
+// Use `Object.entries` to add each key and value pair to the panel
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key}: ${value}`);
+    });
+
+  //buildGauge(result.wfreq)
+
+
+  });
+}
+
+//=============Gauge Chart Function=======================//
+
+function buildGaugeChart(sample) {
+  console.log("sample", sample);
+
+  d3.json("samples.json").then(data =>{
+
+    var objs = data.metadata;
+    //console.log("objs", objs);
+
+    var matchedSampleObj = objs.filter(sampleData => 
+      sampleData["id"] === parseInt(sample));
+    //console.log("buildGaugeChart matchedSampleObj", matchedSampleObj);
+
+    gaugeChart(matchedSampleObj[0]);
+ });   
+}
+
+
+//======================================================//
+//=============== Build a GAUGE Chart ==================//
+//======================================================//
+function gaugeChart(data) {
+  console.log("gaugeChart", data);
+
+  if(data.wfreq === null){
+    data.wfreq = 0;
+
+  }
+
+  let degree = parseInt(data.wfreq) * (180/10);
+
+  // Trig to calc meter point
+  let degrees = 180 - degree;
+  let radius = .5;
+  let radians = degrees * Math.PI / 180;
+  let x = radius * Math.cos(radians);
+  let y = radius * Math.sin(radians);
+
+  let mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+      pathX = String(x),
+      space = ' ',
+      pathY = String(y),
+      pathEnd = ' Z';
+  let path = mainPath.concat(pathX, space, pathY, pathEnd);
+  
+  let trace = [{ type: 'scatter',
+     x: [0], y:[0],
+      marker: {size: 50, color:'2F6497'},
+      showlegend: false,
+      name: 'WASH FREQ',
+      text: data.wfreq,
+      hoverinfo: 'text+name'},
+    { values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+    rotation: 90,
+    text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1',''],
+    textinfo: 'text',
+    textposition:'inside',
+    textfont:{
+      size : 16,
+      },
+    marker: {colors:[...arrColorsG]},
+    labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '2-1', '0-1',''],
+    hoverinfo: 'text',
+    hole: .5,
+    type: 'pie',
+    showlegend: false
+  }];
+
+  let layout = {
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '#2F6497',
+        line: {
+          color: '#2F6497'
+        }
+      }],
+
+    title: '<b>Belly Button Washing Frequency</b> <br> <b>Scrub Per Week</b>',
+    height: 550,
+    width: 550,
+    xaxis: {zeroline:false, showticklabels:false,
+               showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+               showgrid: false, range: [-1, 1]},
+  };
+
+  Plotly.newPlot('gauge', trace, layout, {responsive: true});
+
+}
